057725b23cf8cac5289da5e1c356841c599dc050
Sunim <sunim.54@gmail.com>
2017-05-21 17:54:57 +0545

    Imported model to admin

--- armurl/shortener/models.py
+++ armurl/shortener/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class armURL(models.Model):
-    pass
+    url = models.CharField(max_length=220, )
+
+    def _str_(self):
+        return str(self.url)
+    
+    def _unicode_(self):
+        return str(self.url)
