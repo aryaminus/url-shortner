3049cccc912bec931eb831ab423ec9958bff95ad
Sunim <sunim.54@gmail.com>
2017-05-22 18:12:41 +0545

    Define shortcode length and migrate

--- armurl/shortener/models.py
+++ armurl/shortener/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class armURL(models.Model):
     url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15)
 
     def __str__(self):
         return str(self.url)
