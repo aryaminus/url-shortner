607e716b6076a01e207846309c0330a0735a8a13
Sunim <sunim.54@gmail.com>
2017-05-23 16:57:35 +0545

    Added Timestamp & deleated migrations+db.sqlite & add new Suser

--- armurl/shortener/models.py
+++ armurl/shortener/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class armURL(models.Model):
-    url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)
+    url         = models.CharField(max_length=220, )
+    shortcode   = models.CharField(max_length=15, unique=True)
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return str(self.url)
@@ -11,11 +14,11 @@ class armURL(models.Model):
     def __unicode__(self):
         return str(self.url)
 
-
- '''
+'''
  python manage.py makemigrations
  python manage.py migrate
  python manage.py flush 
+ python manage.py createsuperuser
  
  python manage.py createsuperuser
 '''
