3a42dd6fc21b8e70ec472e1414eaa070c77c80eb
Sunim <sunim.54@gmail.com>
2017-05-25 16:39:42 +0545

    Define Shortcode Blank Condition

--- armurl/shortener/models.py
+++ armurl/shortener/models.py
@@ -1,19 +1,21 @@
 
 from django.db import models
-
+from .utils import code_generator
 # Create your models here.
 
 
 
 class armURL(models.Model):
     url         = models.CharField(max_length=220, )
-    shortcode   = models.CharField(max_length=15, unique=True)
+    shortcode   = models.CharField(max_length=15, unique=True, blank=True)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        print("Something")
-        self.shortcode = code_generator()
+        #print("Something")
+        #self.shortcode = code_generator()
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
         super(armURL, self).save(*args, **kwargs)
 
 
