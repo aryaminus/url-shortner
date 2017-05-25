bb119b0f73446b1c31ba39a3c687c6f46277a194
Sunim <sunim.54@gmail.com>
2017-05-25 16:42:26 +0545

    Checking if shortcode instance is repeated

--- armurl/shortener/utils.py
+++ armurl/shortener/utils.py
@@ -3,3 +3,9 @@ import string
 
 def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+def create_shortcode(instance, size=6):
+    new_code = code_generator(size=size)
+    print(instance)
+    print(instance.__class__)
+    print(instance.__class__.__name__)
