e3e2b21f5bc3a8c19d0b24a8073505225eea71f4
Sunim <sunim.54@gmail.com>
2017-05-27 08:54:33 +0545

    Fixing Typos

--- armurl/shortener/models.py
+++ armurl/shortener/models.py
@@ -3,14 +3,14 @@ from django.db import models
 from .utils import code_generator, create_shortcode
 # Create your models here.
 
-class KirrURLManager(models.Manager):
+class armURLManager(models.Manager):
     def all(self, *args, **kwargs):
-        qs_main = super(KirrURLManager, self).all(*args, **kwargs)
+        qs_main = super(armURLManager, self).all(*args, **kwargs)
         qs = qs_main.filter(active=True)
         return qs
 
 def refresh_shortcodes(self):
-    qs = KirrURL.objects.filter(id__gte=1)
+    qs = armURL.objects.filter(id__gte=1)
     new_codes = 0
     for q in qs:
         q.shortcode = create_shortcode(q)
@@ -26,7 +26,7 @@ class armURL(models.Model):
     timestamp   = models.DateTimeField(auto_now_add=True)
     active      = models.BooleanField(default=True)
 
-    objects = KirrURLManager()
+    objects = armURLManager()
     #some_random = KirrURLManager()
 
     def save(self, *args, **kwargs):
