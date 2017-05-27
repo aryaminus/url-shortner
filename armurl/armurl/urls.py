f74e71d46937288a95cbef8b850303fc7ba341fc
Sunim <sunim.54@gmail.com>
2017-05-27 13:35:14 +0545

    Getting Http response from views.py

--- armurl/armurl/urls.py
+++ armurl/armurl/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from shortener.views import arm_redirect_view, ArmCBView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^view-1/$', arm_redirect_view),
+    url(r'^view-2/$', ArmCBView.as_view()),
 ]
--- armurl/shortener/views.py
+++ armurl/shortener/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def arm_redirect_view(request, *args, **kwargs):
+    return HttpResponse("YO")
+
+class ArmCBView(View):
+    def get(self,request, *args, **kwargs):
+        return HttpResponse("Yo Yo")
