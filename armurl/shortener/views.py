d92a0f80cef00007b62573954011e4ef3de4f948
Sunim <sunim.54@gmail.com>
2017-05-29 10:27:45 +0545

    Rearranging the codes

--- armurl/shortener/views.py
+++ armurl/shortener/views.py
@@ -4,19 +4,30 @@ from django.views import View
 
 from .models import armURL
 
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
 
 def arm_redirect_view(request, shortcode=None, *args, **kwargs):
+    obj = get_object_or_404(armURL, shortcode=shortcode)
+    return HttpResponse("YO {sc}".format(sc= obj.url))
+
+class ArmCBView(View):
+    def get(self,request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(armURL, shortcode=shortcode)
+        return HttpResponse("Yo Yo {sc}".format(sc= shortcode))
+    
+    def post(self,request, *args, **kwargs):
+        return HttpResponse()
+
+   
     '''
     try:
         obj = armURL.objects.get(shortcode=shortcode)
     except:
         obj = armURL.objects.all().first()
     '''
-    obj = get_object_or_404(armURL, shortcode=shortcode)
-    obj_url = obj.url
+    #obj_url = obj.url
     '''
     obj_url =None
     qs = armURL.objects.filter(shortcode_iexact=shortcode.upper())
@@ -24,8 +35,3 @@ def arm_redirect_view(request, shortcode=None, *args, **kwargs):
         obj = qs.first()
         obj_url = obj.url
     '''
-    return HttpResponse("YO {sc}".format(sc= obj.url))
-
-class ArmCBView(View):
-    def get(self,request, shortcode=None, *args, **kwargs):
-        return HttpResponse("Yo Yo {sc}".format(sc= shortcode))
