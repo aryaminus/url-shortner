f3dd77313e3ca7b0b5fa34ad102930d2349b848d
Sunim <sunim.54@gmail.com>
2017-05-29 10:20:04 +0545

    Showing only query set error

--- armurl/shortener/views.py
+++ armurl/shortener/views.py
@@ -9,14 +9,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def arm_redirect_view(request, shortcode=None, *args, **kwargs):
+    '''
     try:
         obj = armURL.objects.get(shortcode=shortcode)
     except:
         obj = armURL.objects.all().first()
-    
+    '''
+    obj_url =None
     qs = armURL.objects.filter(shortcode_iexact=shortcode.upper())
     if qs.exists() and qs.count() == 1:
         obj = qs.first()
+        obj_url = obj.url
+    
     return HttpResponse("YO {sc}".format(sc= obj.url))
 
 class ArmCBView(View):
