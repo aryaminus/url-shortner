a6633dc1398d8c5d2679104894a12dbfda7df9d5
Sunim <sunim.54@gmail.com>
2017-05-29 22:42:26 +0545

    django_hosts pip install, follow readthedocs, create hosts & make root_hostconf

--- armurl/armurl/settings.py
+++ armurl/armurl/settings.py
@@ -38,11 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #custom app
+    'django_hosts',
+
     #Custom App
     'shortener',
 ]
 
 MIDDLEWARE = [
+    'django_hosts.middleware.HostsRequestMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,9 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_hosts.middleware.HostsResponseMiddleware'
 ]
 
 ROOT_URLCONF = 'armurl.urls'
+ROOT_HOSTCONF = 'armurl.hosts'
+DEFAULT_HOST = 'www'
 
 TEMPLATES = [
     {
