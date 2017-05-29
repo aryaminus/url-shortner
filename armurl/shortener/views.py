a7e0e8213c84c6ec393b540447c8350bb980836b
Sunim <sunim.54@gmail.com>
2017-05-29 10:13:17 +0545

    Create Obj URL

--- armurl/shortener/views.py
+++ armurl/shortener/views.py
@@ -2,12 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from .models import armURL
+
 from django.shortcuts import render
 
 # Create your views here.
 
 def arm_redirect_view(request, shortcode=None, *args, **kwargs):
-    return HttpResponse("YO {sc}".format(sc= shortcode))
+    obj = armURL.objects.get(shortcode=shortcode)
+    return HttpResponse("YO {sc}".format(sc= obj.url))
 
 class ArmCBView(View):
     def get(self,request, shortcode=None, *args, **kwargs):
