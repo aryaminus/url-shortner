351aa8736ff28793b5189519c9dce58411512845
Sunim <sunim.54@gmail.com>
2017-05-31 17:35:35 +0545

    Create Html Form and inser post method in views

--- armurl/shortener/views.py
+++ armurl/shortener/views.py
@@ -6,6 +6,12 @@ from .models import armURL
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "shortener/home.html", {}) 
+        
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        print(request.POST["url"])
+        print(request.POST.get("url"))
+        return render(request, "shortener/home.html", {})
 
 class ArmCBView(View):
     def get(self,request, shortcode=None, *args, **kwargs):
