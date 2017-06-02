2fbbef0dffaf2e8d059b655d2fd6f52c7c2ba198
Sunim <sunim.54@gmail.com>
2017-06-02 19:13:20 +0545

    put validator in validators

--- armurl/shortener/forms.py
+++ armurl/shortener/forms.py
@@ -3,4 +3,4 @@ from django import forms
 from .validators import validate_url, validate_dot_com
 
 class SubmitUrlForm(forms.Form):
-    url = forms.CharField(label='Submit URL')
+    url = forms.CharField(label='Submit URL', validators=[validate_url, validate_dot_com])
