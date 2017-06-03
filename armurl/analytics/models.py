a123d10bf841db8db1826f5988f171803bbec6fb
Sunim <sunim.54@gmail.com>
2017-06-03 18:52:43 +0545

    Click Event Setup

--- armurl/analytics/models.py
+++ armurl/analytics/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from shortener.models import armURL
+
+class ClickEvent(models.Model):
+    arm_url    = models.OneToOneField(armURL)
+    count       = models.IntegerField(default=0)
+    updated     = models.DateTimeField(auto_now=True) 
+    timestamp   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{i}".format(i=self.count) 
