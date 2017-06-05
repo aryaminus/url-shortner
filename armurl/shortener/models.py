095e8dabad8abc024e8a7a9534862f5c449d60c1
Sunim <sunim.54@gmail.com>
2017-06-05 17:51:44 +0545

    Add cleanurl for http:// in forms & check if not http then add in models with error handling for http and raising error or returning new value

--- armurl/shortener/models.py
+++ armurl/shortener/models.py
@@ -41,6 +41,8 @@ class armURL(models.Model):
         #self.shortcode = code_generator()
         if self.shortcode is None or self.shortcode == "":
             self.shortcode = create_shortcode(self)
+        if not "http" in self.url:
+            self.url = "http://" + self.url
         super(armURL, self).save(*args, **kwargs)
 
 
